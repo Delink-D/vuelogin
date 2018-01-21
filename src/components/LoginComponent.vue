9c8b72c88ccf8c1af2ed1a6db791fa298bbb767e
Delink-D <delinkdesigns@gmail.com>
2018-01-21 15:58:46 +0300

    added the login form and its validations

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,6 +1,41 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
+  <div class='md-layout md-gutter'>
+    <div class='md-layout-item'></div>
+    <md-card class='md-layout-item login-card'>
+
+      <md-card-header class='login-card--header'>
+        <div class='md-title'>Login App</div>
+      </md-card-header>
+
+      <md-card-content class='login-card--body'>
+        <form novalidate class="md-layout-row md-gutter">
+
+          <md-field :class="getValidationClass('email')">
+            <label for="email">Email</label>
+            <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email" :disabled="sending" />
+            <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
+            <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
+          </md-field>
+
+          <md-field :class="getValidationClass('password')">
+            <label for="password">Password</label>
+            <md-input type="password" name="password" id="password" v-model="form.password" :disabled="sending" />
+            <span class="md-error" v-if="!$v.form.password.required">The Password is required</span>
+            <span class="md-error" v-else-if="!$v.form.password.minLength">Your password is shorter</span>
+          </md-field>
+
+          <md-checkbox v-model="boolean" class="md-primary">Remember Me</md-checkbox>
+        </form>
+      </md-card-content>
+
+      <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
+      <md-card-actions class='md-alignment-left'>
+        <md-button type="submit" class="md-raised md-primary" :disabled="sending">Login</md-button>
+      </md-card-actions>
+
+    </md-card>
+    <div class='md-layout-item'></div>
   </div>
 </template>
 
@@ -10,9 +45,38 @@ import { required, email, minLength, maxLength } from 'vuelidate/lib/validators'
 
 export default {
   name: 'LoginComponet',
-  data () {
-    return {
-      msg: 'Login componet'
+  mixins: [validationMixin],
+  data: () => ({
+    form: {
+      email: null,
+      password: null
+    },
+    sending: false
+  }),
+  validations: {
+    form: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      }
+    }
+  },
+  methods: {
+    getValidationClass (fieldName) {
+      const field = this.$v.form[fieldName]
+
+      if (field) {
+        return {
+          'md-invalid': field.$invalid && field.$dirty
+        }
+      }
+    },
+    loginUser() {
+      this.sending = true
     }
   }
 }
