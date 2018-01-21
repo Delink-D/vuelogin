3cf835f8efcaf6ccf8c7b0f54ac253481a0cb703
Delink-D <delinkdesigns@gmail.com>
2018-01-21 22:07:32 +0300

    check if a user is currently logged in before navigation and redirect to home else login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import firebase from 'firebase'
 import LoginComponent from '@/components/LoginComponent'
 import HomeComponent from '@/components/HomeComponent'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
+      path: '*',
+      redirect: '/login'
+    },
+    {
       path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
       name: 'LoginComponent',
       component: LoginComponent
     },
     {
       path: '/home',
       name: 'HomeComponent',
-      component: HomeComponent
+      component: HomeComponent,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  let currentUser = firebase.auth().currentUser
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+  if (requiresAuth && !currentUser) next ('login')
+  else if (!requiresAuth && currentUser) next ('home')
+  else next ()
+})
+
+export default router 
