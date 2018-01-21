31abc33213723da72b576a161e4056a815df01a7
Delink-D <delinkdesigns@gmail.com>
2018-01-21 21:13:38 +0300

    Initialized the firebase on the main file to the app

--- src/main.js
+++ src/main.js
@@ -4,10 +4,23 @@ import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 import environment from '../config/environment'
 
 Vue.config.productionTip = false
 Vue.use(VueMaterial)
+
+// Initialize Firebase
+var config = {
+  apiKey: environment.firebase.apiKey,
+  authDomain: environment.firebase.authDomain,
+  databaseURL: environment.firebase.databaseURL,
+  projectId: environment.firebase.projectId,
+  storageBucket: environment.firebase.storageBucket,
+  messagingSenderId:environment.firebase.messagingSenderId 
+};
+
+firebase.initializeApp(config);
 
 /* eslint-disable no-new */
 new Vue({
